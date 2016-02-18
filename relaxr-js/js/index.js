6f63b8874f7042e797f9f88f3aea02e3c6971655
Rita Troyer <ritatroyer@gmail.com>
2016-02-18 14:49:50 -0800

    adding js to relaxr

---
+++ relaxr-js/js/index.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+//begin doc ready	
+
+$('#readmore-btn').click(function(e){ 
+	e.preventDefault(); 
+	$('.readmore').hide(); 
+	$('#show-this-on-click').slideDown('slow');
+	$('.readless').show(); 
+});
+
+$('#readless-btn').click(function(e){ 
+	e.preventDefault(); 
+	$('#show-this-on-click').slideUp('slow', function(){
+	$('.readless').hide();
+	$('.readmore').show();
+});	
+
+});
+
+//learn more
+$('.learnmore-btn').click(function(e){
+	e.preventDefault()
+	$('#learnmoretext').slideDown('slow', function(){
+	$('.learnmore-btn').hide();
+});
+
+});
+
+//sign up
+
+$('#bottom-cta a').click(function(e){
+	e.preventDefault()
+});	
+
+//end doc ready
+});
